271d93fb34139c9d6baaabdd85e3177a56275491
Nazarii Proshyn <nproshyn@corelogic.com>
2021-03-24 18:34:13 +0200

    Fix comments5

--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
 #include "threadSafeQueue.h"
 
-
-void foo1() {
-    std::cout << "One\n";
-}
-
-void foo2() {
-    std::cout << "Two\n";
-}
-
-void foo3() {
-    std::cout << "Three\n";
-}
-
 int main()
 {
     return 0;
 }
-
-
