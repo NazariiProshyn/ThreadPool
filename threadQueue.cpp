f6f16a51a56c5a53359aebf9f9ed7cd2a061facf
Nazarii Proshyn <nproshyn@corelogic.com>
2021-03-24 14:21:09 +0200

    Thread-safe-queue

---
+++ threadQueue.cpp
@@ -0,0 +1,22 @@
+#include "threadQueue.h"
+
+
+void ThreadQueue::push(std::function<void()> value)
+{
+	std::lock_guard<std::mutex> lock(mut);
+	dataQueue.push(std::move(value));
+	dataCondition.notify_one();
+}
+
+
+std::function<void()> ThreadQueue::pop()
+{
+	std::unique_lock<std::mutex> lock(mut);
+	dataCondition.wait(lock, [this] {return !dataQueue.empty(); });
+
+	std::function<void()> value = std::move(dataQueue.front());
+	dataQueue.pop();
+	return value;
+}
+
+
