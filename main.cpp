4281ad44ad388da800a9b1543212fd46c84c0f62
NazariyProshyn <56487679+NazariyProshyn@users.noreply.github.com>
2021-03-25 19:21:19 +0200

    Implement thread pool (#3)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include "threadSafeQueue.h"
+#include "threadPool.h"
+#include <iostream>
 
+void foo1() {
+    std::cout << "One\n";
+}
+
+void foo2() {
+    std::cout << "Two\n";
+}
+
+void foo3() {
+    std::cout << "Three\n";
+}
 
 int main()
 {
+    ThreadPool threadpool(4);
+
+    for (size_t i = 0; i < 3; ++i)
+    {
+        threadpool.submit(foo1);
+        threadpool.submit(foo2);
+        threadpool.submit(foo3);
+    }
+
     return 0;
 }
+
