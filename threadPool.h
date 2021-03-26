8ed7fdfd9bd8f34cc47e20f9e36fa3e0ff39d073
Nazarii Proshyn <nproshyn@corelogic.com>
2021-03-26 18:38:59 +0200

    Add spaces

--- threadPool.h
+++ threadPool.h
@@ -21,31 +21,31 @@
 class ThreadPool
 {
 public:
-	//! \brief                        Creates the specified number of threads         
-	//! \param [in] thread_count      Number of threads to be created
-	ThreadPool(const size_t thread_count);
-	~ThreadPool();
+    //! \brief                        Creates the specified number of threads         
+    //! \param [in] thread_count      Number of threads to be created
+    ThreadPool(const size_t thread_count);
+    ~ThreadPool();
 
-	//Thread not copiable
-	ThreadPool(const ThreadPool&) = delete;
-	ThreadPool& operator=(const ThreadPool&) = delete;
+    //Thread not copiable
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
 
-	//! \brief                 Add function to the execution queue  
-	//! \param [in] funct      Function that is added to the execution queue
-	//! \return                   
-	void submit(const std::function<void()> funct);
+    //! \brief                 Add function to the execution queue  
+    //! \param [in] funct      Function that is added to the execution queue
+    //! \return                   
+    void submit(const std::function<void()> funct);
 
 private:
 	//Vector of working threads
 	std::vector<std::thread> threads;
 
-	//Flag that indicate the
-	//possibility of obtaining a task
-	std::atomic_bool run{true};
+    //Flag that indicate the
+    //possibility of obtaining a task
+    std::atomic_bool run{true};
 
-	//Thread-safe queue of tasks
-	ThreadSafeQueue workQueue;
+    //Thread-safe queue of tasks
+    ThreadSafeQueue workQueue;
 
-   //! \brief        Runs functions from the queue
-	void workThread();
+    //! \brief        Runs functions from the queue
+    void workThread();
 };
